71f34197a5a6ab80ea3b64f1bce6fd91287dcdfb
anahi <luna.calderon@mi.unc.edu.ar>
2024-10-05 17:35:52 -0300

    Crea una aplicación para consultar películas de Star Wars mediante SWAPI

---
+++ src/Pelicula.java
@@ -0,0 +1,7 @@
+public record Pelicula(String title,
+                       int  espisode_id,
+                       String opening_crawl,
+                       String director,
+                       String producer,
+                       String release_date) {
+}
